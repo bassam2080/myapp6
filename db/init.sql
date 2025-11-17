71b9b1f5a36b5316148d75424039f96357acd4d9
bassam2080 <bassam20_152@yahoo.com>
2025-11-17 11:03:32 +0000

    Update init.sql

---
+++ db/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS Test (
+  Id   INT PRIMARY KEY,
+  Name VARCHAR(45)
+);
+
+INSERT INTO Test (Id, Name) VALUES
+(1, 'Ross'),
+(2, 'Jonathan'),
+(3, 'Lauren'),
+(4, 'sarah'),
+(5, 'Drew');
